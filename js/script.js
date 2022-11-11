2908916a90957108b09989fa169f6394dbaad2b1
VitorPalermo <101677215+VitorPalermo@users.noreply.github.com>
2022-11-10 21:49:01 -0300

    img_rotativas

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 /*carrocel das imagens(rolagem) */
-const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
-    // Your options go here
-  });
+const carouselDestaque = new Carousel(document.querySelector("#carousel-destaque"), {
+  // Your options go here
+});
+
+/*carrocel das imagens(rolagem) */
+const carouselColar = new Carousel(document.querySelector("#carousel-colar"), {
+  // Your options go here
+});
+
+/*carrocel das imagens(rolagem) */
+const carouselBrincos = new Carousel(document.querySelector("#carousel-brincos"), {
+  // Your options go here
+});
+
+/*carrocel das imagens(rolagem) */
+const carouselPulseiras = new Carousel(document.querySelector("#carousel-pulseiras"), {
+  // Your options go here
+});
+
